5de157f1520ae8778b8a29a3b0d7b0d5804b690e
Ivan Santiago Di Gruttola <ivansandigruttola@gmail.com>
2021-03-30 17:04:33 -0300

    add component GUI jLabel

---
+++ Avanced/GUI/jLabel/src/Label/Main.java
@@ -0,0 +1,11 @@
+package Label;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// JLabel = a GUI display area for a string of text, an image , or both
+		
+		new Method();
+	}
+	
+}
