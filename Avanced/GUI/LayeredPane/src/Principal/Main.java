295f3efe40ff5f24da095b69198684e1691647e9
Ivan Santiago Di Gruttola <ivansandigruttola@gmail.com>
2021-04-01 23:05:43 -0300

    LayeredPane

---
+++ Avanced/GUI/LayeredPane/src/Principal/Main.java
@@ -0,0 +1,18 @@
+package Principal;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * LayeredPane = swing container that provides a 
+		 * 				 third dimension for positioning components
+		 * 				 ex. depth, Z-index
+		 * 
+		 * */
+		 
+		
+		new GUI();
+	}
+	
+}
