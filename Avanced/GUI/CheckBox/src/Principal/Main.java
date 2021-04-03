cc60bc46b5051108e6fb4a4dec7b6b4456300597
Ivan Santiago Di Gruttola <ivansandigruttola@gmail.com>
2021-04-03 11:03:24 -0300

    CheckBox

---
+++ Avanced/GUI/CheckBox/src/Principal/Main.java
@@ -0,0 +1,16 @@
+package Principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * JCheckBox = A GUI component that can be selected or deselected
+		 * 
+		 * */
+		
+		new CheckB();
+	}
+
+}
